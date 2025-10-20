58087101c1c3473c469fb2347111d72f3665307e
PinoskiML <felipe.montagne.lascaris@gmail.com>
2025-10-20 15:54:20 +0200

    set

---
+++ src/main/java/com/IH/ExamenFelipeMontagne/ExamenFelipeMontagneApplication.java
@@ -0,0 +1,13 @@
+package com.IH.ExamenFelipeMontagne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFelipeMontagneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFelipeMontagneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IH/ExamenFelipeMontagne/ExamenFelipeMontagneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IH.ExamenFelipeMontagne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFelipeMontagneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
